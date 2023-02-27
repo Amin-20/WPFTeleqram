b711c62edb8419e31c206f9a8549c566f15df345
Ataki_pw11 <Ataki_pw11@STHQ0127-05>
2023-02-27 16:24:57 +0400

    Seach model done

--- WPFTeleqram/MainWindow.xaml.cs
+++ WPFTeleqram/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,11 @@ namespace WPFTeleqram
     /// </summary>
     public partial class MainWindow : Window
     {
+   
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
